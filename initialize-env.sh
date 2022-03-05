98623c21cb55057b6cf1d872f9e86fda5a8928ed
Dan Dowd <danieldowd89@gmail.com>
2022-03-05 16:40:55 -0700

    updated workspace-3 layout

---
+++ initialize-env.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+req_dirs=("/home/$USER/Work" "/home/$USER/.i3" "/home/$USER/.i3/layouts")
+
+for dir in ${req_dirs[@]}; do
+	if [ ! -d $dir ]
+	then
+		echo "creating $dir"
+		mkdir $dir
+	else
+		echo "$dir already exists"
+	fi
+done
