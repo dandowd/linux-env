c559daad2bbbe5617e145e3d0a69f7220dbcbc19
Dan Dowd <danieldowd89@gmail.com>
2025-09-26 11:44:35 -0600

    adds vscode

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,16 @@
-require("package-manager")
-require("settings")
-require("remap")
+if vim.g.vscode then
+  -- no op for now
+else
+  require("package-manager")
+  require("settings")
+  require("remap")
 
--- Defer non-essential configs
-vim.defer_fn(function()
-	require("treesitter-config")
-	require("telescope-config") 
-	require("dap-config")
-	require("plugins")
-	require("macros")
-end, 0)
+  -- Defer non-essential configs
+  vim.defer_fn(function()
+    require("treesitter-config")
+    require("telescope-config") 
+    require("dap-config")
+    require("plugins")
+    require("macros")
+  end, 0)
+end
